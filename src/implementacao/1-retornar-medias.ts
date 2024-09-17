48d1e1d81f537c585f33ddb618b4ace6e6b2cee9
mylenaverspeelt <mylena49@hotmail.com>
2024-09-17 11:46:42 -0300

    feat: Resposta do Exercicio 1

--- src/implementacao/1-retornar-medias.ts
+++ src/implementacao/1-retornar-medias.ts
@@ -1,3 +1,11 @@
-function retornarMedias(){
-    return null
+
+import { Alunas, turma } from "../../listaPreConstruida";
+
+function retornarMedias(turma: Alunas): Array<number> {
+
+    const medias :number[] = turma.map((item) =>
+        parseFloat(((item.prova.p1 + item.prova.p2 + item.prova.p3) / 3).toFixed(1)))
+
+    return medias
 }
+console.log(retornarMedias(turma));
