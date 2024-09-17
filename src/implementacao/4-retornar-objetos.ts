ce881ffc5990a8307c8267e859f900050e5ec7b3
mylenaverspeelt <mylena49@hotmail.com>
2024-09-17 16:01:08 -0300

    feat: Resposta do Exercicio 4 parte 1

--- src/implementacao/4-retornar-objetos.ts
+++ src/implementacao/4-retornar-objetos.ts
@@ -1,3 +1,35 @@
-function retornarObjetos() {
-    return null;
+import { Alunas, turma } from "../../listaPreConstruida";
+
+interface AprovadasEReprovadas {
+    nome: String;
+    media: Number;
+    aprovada: Boolean;
 }
+
+function retornarObjetos(turma: Alunas): AprovadasEReprovadas[] {
+
+    const arrayMedias: Array<AprovadasEReprovadas> = turma.map((aluna) => {
+
+        let media: number = parseFloat(((aluna.prova.p1 + aluna.prova.p2 + aluna.prova.p3) / 3).toFixed(1))
+
+        let status: boolean = false
+
+        if (media >= 7) {
+            status = true
+        }
+
+        let objeto:AprovadasEReprovadas = {
+            nome: aluna.nome,
+            media: media,
+            aprovada: status
+        }
+        return objeto
+
+    })
+
+    console.log(arrayMedias);
+
+    return []
+
+}
+console.log(retornarObjetos(turma))
