c3619f4319965914f42eb74495ce4573ed6bb260
Marvyson Cardoso <marvysonprog@gmail.com>
2024-09-17 13:56:36 -0300

    feat: Resposta do Exercicio3

--- src/implementacao/3-retornar-reprovadas.ts
+++ src/implementacao/3-retornar-reprovadas.ts
@@ -1,3 +1,13 @@
-function retornarReprovadas() {
-    return null;
+import { Alunas, turma } from "../../listaPreConstruida";
+
+function retornarReprovadas(turma: Alunas): String[] {
+  const reprovadas: Alunas = turma.filter(
+    (reprovada) =>
+      (reprovada.prova.p1 + reprovada.prova.p2 + reprovada.prova.p3) / 3 < 7
+  );
+
+  const nomesReprovadas = reprovadas.map((reprovada) => reprovada.nome);
+
+  return nomesReprovadas;
 }
+console.log(retornarReprovadas(turma));
