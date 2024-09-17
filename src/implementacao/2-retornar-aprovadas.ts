5b2d73ab267a1d2030bf1ee2a57427afef96355d
Marvyson Cardoso <marvysonprog@gmail.com>
2024-09-17 13:32:29 -0300

    feat: Resposta do Exercicio2

--- src/implementacao/2-retornar-aprovadas.ts
+++ src/implementacao/2-retornar-aprovadas.ts
@@ -1,4 +1,12 @@
-function retornarAprovadas() {
-    return null;
-}
+import { Alunas, turma } from "../../listaPreConstruida";
 
+function retornarAprovadas(turma: Alunas): String[] {
+  const vetorDeAprovadas: Alunas = turma.filter(
+    (aluna) => (aluna.prova.p1 + aluna.prova.p2 + aluna.prova.p3) / 3 >= 7
+  );
+
+  const nomeAprovadas: String [] = vetorDeAprovadas.map((aprovada) => aprovada.nome);
+
+  return nomeAprovadas;
+}
+console.log(retornarAprovadas(turma));
