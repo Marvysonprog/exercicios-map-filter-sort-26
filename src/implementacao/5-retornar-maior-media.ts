a8102f99a10da541126de28bc12be9021ede2f8c
Marvyson Cardoso <marvysonprog@gmail.com>
2024-09-18 13:37:44 -0300

    feat: Resposta do Exercicio 5

--- src/implementacao/5-retornar-maior-media.ts
+++ src/implementacao/5-retornar-maior-media.ts
@@ -1,3 +1,20 @@
-function retornarMaiorMedia() {
-    return null;
+import { Alunas, turma } from "../../listaPreConstruida";
+
+function retornarMaiorMedia(turma: Alunas): String {
+  let maiorMedia: number = 0;
+  let nomeAlunaMaiorMedia: String = "";
+
+  const arrayDeMedia: void [] = turma.map((aluna) => {
+    const media: number =
+      (aluna.prova.p1 + aluna.prova.p2 + aluna.prova.p3) / 3;
+
+    if (media > maiorMedia) {
+      maiorMedia = media;
+      nomeAlunaMaiorMedia = aluna.nome
+    }
+
+  });
+
+  return nomeAlunaMaiorMedia;
 }
+console.log(retornarMaiorMedia(turma));
