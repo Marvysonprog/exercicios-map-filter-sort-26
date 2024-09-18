2b9d3efbcdd30e6a464785250e3b5a3ad250b7af
Marvyson Cardoso <marvysonprog@gmail.com>
2024-09-18 16:00:14 -0300

    feat: Resposta do Exercicio 7

--- src/implementacao/7-retornar-media-turma.ts
+++ src/implementacao/7-retornar-media-turma.ts
@@ -1,3 +1,17 @@
-function retornarMediaTurma() {
-    return null;
+import { Alunas, turma } from "../../listaPreConstruida";
+
+function retornarMediaTurma(turma: Alunas): number {
+  let valorAcumulado: number = 0;
+
+  const arrayDeMedia: number[] = turma.map(
+    (aluna) => (aluna.prova.p1 + aluna.prova.p2 + aluna.prova.p3) / 3
+  );
+
+  arrayDeMedia.map((media) => (valorAcumulado += media));
+  const mediaDaTurma: number = parseFloat(
+    (valorAcumulado / turma.length).toFixed(1)
+  );
+
+  return mediaDaTurma;
 }
+console.log(retornarMediaTurma(turma));
