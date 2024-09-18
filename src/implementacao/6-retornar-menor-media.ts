30755484aaa755eb3c8b71351e986314527cc447
mylenaverspeelt <mylena49@hotmail.com>
2024-09-18 15:32:14 -0300

    feat: Resposta do Exercicio7

--- src/implementacao/6-retornar-menor-media.ts
+++ src/implementacao/6-retornar-menor-media.ts
@@ -1,3 +1,22 @@
-function retornarMenorMedia() {
-    return null;
+import { Alunas, turma } from "../../listaPreConstruida";
+
+function retornarMenorMedia(turma: Alunas): String {
+    let nomeAlunaMenorMedia: String = ""
+
+    const arrayMedias: number[] = turma.map((aluna) => {
+        return ((aluna.prova.p1 + aluna.prova.p2 + aluna.prova.p3) / 3)
+    })
+
+    const arrayMediasOrdenado: number[] = arrayMedias.sort()
+
+    const menorMedia: number = arrayMediasOrdenado[0]
+
+    turma.filter((aluna) => {
+        if (((aluna.prova.p1 + aluna.prova.p2 + aluna.prova.p3) / 3) == menorMedia) {
+            nomeAlunaMenorMedia = aluna.nome
+        }
+    })
+    return nomeAlunaMenorMedia
 }
+console.log(retornarMenorMedia(turma))
+
